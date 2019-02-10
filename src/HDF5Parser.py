65d5494c8f0d1a114a7f5a336835a949448637c4
Sarah Aguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2019-02-10 15:43:08 -0700

    recursive calling objects

--- src/HDF5Parser.py
+++ src/HDF5Parser.py
@@ -9,13 +9,27 @@
 import h5py
 import numpy as np
 
-def print_attrs(name, obj):
-    print(name)
-    for key, val in obj.attrs.items():
-        print(key, val)
+filename= '../data/weights.best.hdf5'
+f= h5py.File(filename, 'r')
+
+def recursiveKeyPrinting(key, identifier):
+    try:
+        if str(key.id) in identifier:
+            return
+        else:
+            identifier.append(str(key.id))
+            if isinstance(key, h5py.Dataset):
+                return recursiveKeyPrinting(key.parent, identifier)
+
+            for kkey in key.keys():
+                return recursiveKeyPrinting(key[kkey], identifier)
+
+    except Exception as e:
+        print(str(e))
+        print(identifier)
 
 
-if __name__=="__main__":
-    filename= '../data/weights.best.hdf5'
-    f= h5py.File(filename, 'r')
-    f.visititems(print_attrs)
+if __name__=='__main__':
+    identifier= []
+    recursiveKeyPrinting(f, identifier)
+
