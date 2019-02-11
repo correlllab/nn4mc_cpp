2b07db7d14aa681108d60c7db391e91aa8b30869
Dana Hughes <danathughes@gmail.com>
2019-02-11 17:19:56 -0500

    Simplifying implementation of Tensor

---
+++ include/datastructures/tensor.h
@@ -0,0 +1,65 @@
+/**
+* \file tensor.h
+* Header file defining the Tensor class and associated Exceptions 
+*
+* \class Tensor
+*
+* \brief Light wrapper for tensors
+*
+* This class is meant to be used as a wrapper for Tensors of arbitrary
+* dimensionality.  It does *not* define Tensor operations.  Rather, it
+* simply acts as a means to store and access values in a Tensor without
+* having to deal with underlying C++ details for memory access.
+*
+* \author $Author: Dana Hughes $
+*
+* \version $Version: 0.1 $
+*
+* \date $Date: 2019/02/07 $
+*
+* Contact: danahugh@andrew.cmu.edu
+*
+* Created on: Thu Feb 07 2019
+*
+*/
+
+#ifndef __TENSOR_H__
+#define __TENSOR_H__
+
+#include <cstdarg>
+#include <exception>
+#include <vector>
+
+template <class DataType>
+class Tensor
+{
+	private:
+		std::vector<unsigned int> offsets;
+		unsigned int num_elements;
+
+		DataType* data;
+
+		unsigned int array_index(unsigned int, va_list);
+
+	public:
+		std::vector<unsigned int> shape;
+
+		Tensor(std::vector<unsigned int>);
+		~Tensor();
+
+		DataType& operator()(unsigned int, ...);
+		DataType operator() (unsigned int, ...) const;
+};
+
+class BadIndexBoundsException : public std::exception
+{
+	public:
+		BadIndexBoundsException(unsigned int, unsigned int, unsigned int);
+		~BadIndexBoundsException();
+		unsigned int dimension_number;
+		unsigned int index_bound;
+		unsigned int index;
+		virtual const char* what() const throw();
+};
+
+#endif
