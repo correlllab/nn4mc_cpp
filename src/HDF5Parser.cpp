5a74d2914cb31994b3095f679dee76b20a0d1977
sarahaguasvivas <saag5228@colorado.edu>
2019-02-12 08:51:22 -0700

    imported source code h5cpp

--- src/HDF5Parser.cpp
+++ src/HDF5Parser.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
-#include <h5cpp/hdf5.hpp> 
-#include "../include/tensor.h"
+#include "hdf5/include/hdf5.h" 
+//#include "../include/tensor.h"
 #include "../include/NeuralNetwork.h"
-
-using namespace hdf5;
 
 int main(){
 
-    auto File= file::open("../data/weights.best.hdf5");
-    auto RootGroup = File.root();
-    auto Dataset = RootGroup.get_dataset("conv1d_1");
    
      
     
