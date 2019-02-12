821fc83d0e545cb68c9584ae432ebf0312bf8dde
sarahaguasvivas <saag5228@colorado.edu>
2019-02-12 11:12:08 -0700

    starting to write

--- src/HDF5Parser.cpp
+++ src/HDF5Parser.cpp
@@ -8,13 +8,27 @@
 using namespace H5;
 
 const H5std_string FILE_NAME("../data/weights.best.hdf5");
-const H5std_string DATASET_NAME("IntArray")
-
+const H5std_string DATASET_NAME("IntArray");
+const int NX_SUB = 3;
+const int NY_SUB = 4;
+const int NX = 7;
+const int NY = 7;
+const int NZ = 3;
+const int RANK_OUT = 3;
 
 int main(){
+    std::cout<<"here"<<std::endl;
+        int i, j, k;
+        int data_out[NX][NY][NZ];
+        
+        for (j=0; j < NX; j++){
+            for (i=0; i<NY; i++){ 
+                for (k=0; k< NZ; k++){
+                  data_out[j][i][k] = 0;
+                }
+            } 
+        }
 
-           
-     
     
     return 0;
 }
--- src/README.md
+++ src/README.md
@@ -1,15 +1,5 @@
-### Source File
+### src
 
-I went like:
+To run the parser, I go like:
 
-> https://github.com/ess-dmsc/h5cpp
-
-On Mac:
-> brew install hdf5
-> brew install boost
-
-To run HDF5Parser.cpp we need to go like:
-> g++ -std=c++11 -lboost_system HDF5Parser.cpp
-
-
-I had trouble installing this package on Linux Mint
+> $h5c++ program_name.cpp
