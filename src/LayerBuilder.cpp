da25730d6477ac98fecd33ae68428716fb2aa4fb
sarahaguasvivas <saag5228@colorado.edu>
2019-03-14 14:48:47 -0600

    most layers builders

--- src/LayerBuilder.cpp
+++ src/LayerBuilder.cpp
@@ -11,6 +11,7 @@ void Conv1DBuilder::create_from_json(json obj, std::string id){
     this->layerObject.dilation_rate= object["dilation_rate"][0];
     this->layerObject.activation.assign(object["activation"]);
     this->layerObject.use_bias = object["use_bias"];
+    std::cout<< "Conv1D Layer Built!"<<std::endl;
 }
 
 void Conv2DBuilder::create_from_json(json obj, std::string id){
@@ -20,13 +21,14 @@ void Conv2DBuilder::create_from_json(json obj, std::string id){
     for (int i=0; i<dim_vars; i++){
         this->layerObject.kernel_size.push_back(object["kernel_size"][i]);
         this->layerObject.strides.push_back(object["strides"][i]);
-        this->dilation_rate.push_back(object["dilation_rate"][i]);
+        this->layerObject.dilation_rate.push_back(object["dilation_rate"][i]);
     }
 
     this->layerObject.padding.assign(object["padding"]);
     this->layerObject.data_format.assign(object["data_format"]);
     this->layerObject.activation.assign(object["activation"]);
-    this->use_bias= object["use_bias"];
+    this->layerObject.use_bias= object["use_bias"];
+    std::cout<< "Conv2D Layer Built!"<<std::endl;
 }
 
 void MaxPooling1DBuilder::create_from_json(json obj, std::string id){
@@ -36,7 +38,8 @@ void MaxPooling1DBuilder::create_from_json(json obj, std::string id){
     this->layerObject.pool_size= object["pool_size"][0];
     this->layerObject.strides = object["strides"][0];
     this->layerObject.padding.assign(object["padding"]);
-    this->layerObject.data_format.assign(object["data_format"])
+    this->layerObject.data_format.assign(object["data_format"]);
+    std::cout<<"MaxPooling1D Layer Built!"<<std::endl;
 
 }
 
@@ -50,20 +53,22 @@ void MaxPooling2DBuilder::create_from_json(json obj, std::string id){
     }
     this->layerObject.padding.assign(object["padding"]);
     this->layerObject.data_format.assign(object["data_format"]);
+    std::cout<< "MaxPooling2D Layer Built!"<<std::endl;
 }
 
 void DenseBuilder::create_from_json(json obj, std::string id){
     json object= obj["config"];
     
     this->layerObject.identifier.assign(id);
-    this->layerObject.units=object["units"];
+  /*  this->layerObject.units=object["units"];
     this->layerObject.activation.assign(object["activation"]);
-    this->layerObject.use_bias = object["use_bias"];
-
+    this->layerObject.use_bias = object["use_bias"];*/
+    std::cout<< "Dense Layer Built!"<<std::endl;
 }
 
 void FlattenBuilder::create_from_json(json obj, std::string id){
     this->layerObject.identifier.assign(id);
+    std::cout<< "Flattening Layer Built!"<<std::endl;
 }
 
 void GRUBuilder::create_from_json(json obj, std::string id){
