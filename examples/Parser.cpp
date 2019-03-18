b527a3b31487166b0bf46d14ec4860d501866eec
sarahaguasvivas <saag5228@colorado.edu>
2019-03-18 12:47:41 -0600

    TODO: Plug in the Weights to the Layer Types

--- examples/Parser.cpp
+++ examples/Parser.cpp
@@ -8,10 +8,11 @@ int main(){
     P.parse();
     
     // Printing list of layer_ids:
+    std::cout<< "EXAMPLE: Calling list of Layer IDs"<<std::endl;
     for (int i=0; i<P.layer_ids.size(); i++){
         std::cout << P.layer_ids[i]<< std::endl;
     }
-
+    std::cout << "EXAMPLE: Calling layer.identifier"<<std::endl;
     // Accessing layer objects
     for (int i=0; i<P.layer_ids.size(); i++){
         std::cout<< P.layerMap[P.layer_ids[i]]->identifier <<std::endl;
