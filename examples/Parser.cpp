57c1e326ece4ec8e15cf091a64ac700c33a43af1
sarahaguasvivas <saag5228@colorado.edu>
2019-03-22 13:38:42 -0600

    better example

--- examples/Parser.cpp
+++ examples/Parser.cpp
@@ -6,17 +6,10 @@ int main(){
     
     HDF5Parser P("../data/weights.best.hdf5");
     P.parse();
-    
-    // Printing list of layer_ids:
-    std::cout<< "EXAMPLE: Calling list of Layer IDs"<<std::endl;
-    for (int i=0; i<P.layer_ids.size(); i++){
-        std::cout << P.layer_ids[i]<< std::endl;
-    }
-    std::cout << "EXAMPLE: Calling layer.identifier"<<std::endl;
 
     // Access NeuralNetwork nodes with: P.NN
     
-    P.NN.BFS();
+    P.NN.BFS(); // for example
 
 
     return 0;
