b69728249746580bd218fedc5bab828042903e80
sarahaguasvivas <saag5228@colorado.edu>
2019-08-27 16:50:59 -0600

    no need for environment file

--- examples/file_creator_test.cpp
+++ examples/file_creator_test.cpp
@@ -1,10 +1,10 @@
 #include "generator/file_creator.h"
 #include <iostream>
+#include <cstdlib>
 #include <string>
 
 int main()
 {
-    std::cout << std::getenv("DUMP_PATH_ROOT")<<std::endl;
     std::string path = "generator_test_v2";
     std::cout << path << std::endl; 
     FileCreator file_creator(path);
