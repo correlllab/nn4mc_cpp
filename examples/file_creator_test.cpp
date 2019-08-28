466e9bad3a0f6f036c740b472a57ff2afade65b9
sarahaguasvivas <saag5228@colorado.edu>
2019-08-28 15:31:15 -0600

    file creator: TODO: develop createFile()

--- examples/file_creator_test.cpp
+++ examples/file_creator_test.cpp
@@ -8,7 +8,8 @@ int main()
     std::string path = "generator_test_v2";
     std::cout << path << std::endl; 
     FileCreator file_creator(path);
-    std::cout << file_creator.checkExists() << std::endl;;
+    std::cout << file_creator.checkExists() << std::endl;
+    file_creator.createFile();
     std::cout << "FileCreator: end of file creator example " << std::endl;
 	return 0;
 }
