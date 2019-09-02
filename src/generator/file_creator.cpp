f633507f0969926aa3b2863e5f426e1931303d91
Sarah Aguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2019-09-01 18:50:22 -0600

    finished file creator branch

--- src/generator/file_creator.cpp
+++ src/generator/file_creator.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "generator/file_creator.h"
-
+#include <sstream>
 /*
 FileCreator::FileCreator()
 {
     std::cout << "file_creator.cpp: zero argument constructor" << std::endl;
 }
 */
+
 FileCreator::FileCreator(std::string path_string){
     boost::filesystem::path path{path_string};
     boost_path = path;
@@ -26,9 +27,19 @@ bool FileCreator::checkExists(){
 }
 
 void FileCreator::createDirectory(){
-    
-    boost::filesystem::create_directory(boost_path);                
 
+    if (this->checkExists() == true){
+        std::cout << "file existed so created new file!" <<std::endl;
+        int i=0;
+        while (boost::filesystem::exists(boost_path)){
+            i++;
+            std::stringstream ss;
+            ss<< boost_path.string() << "_" << i;
+            boost_path = ss.str();
+        }
+    }
+    boost::filesystem::create_directory(boost_path);                
+    
 }
 
 
