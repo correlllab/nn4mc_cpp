e572b9614be423e68778411db0d061af742797c4
sarahaguasvivas <saag5228@colorado.edu>
2019-09-03 15:23:58 -0600

    changed cmakelists to have right linkages

--- examples/generator_test_actual_file.cpp
+++ examples/generator_test_actual_file.cpp
@@ -11,29 +11,29 @@
 #include "datastructures/Layer.h"
 #include "datastructures/NeuralNetwork.h"
 
-int main(int argc, char** argv)
+int main()
 {
 
     HDF5Parser P("../data/weights.best.hdf5");
     P.parse();
-	NeuralNetwork* nn = P.constructNeuralNetwork();
-	nn->BFS();
+    NeuralNetwork* nn = P.constructNeuralNetwork();
+    nn->BFS();
 
-	std::cout << "  Neural Network: " << std::endl;
-	std::cout << "  Num Layers: " << (nn->layers).size() << std::endl;
-	std::cout << "  Num Ordered Layers: " << (nn->nodes_ord).size() << std::endl;
-	std::cout << "  Num Input Layers: " << (nn->input).size() << std::endl;
-	std::cout << "  Num Weights: " << (nn->weights).size() << std::endl;
+    std::cout << "  Neural Network: " << std::endl;
+    std::cout << "  Num Layers: " << (nn->layers).size() << std::endl;
+    std::cout << "  Num Ordered Layers: " << (nn->nodes_ord).size() << std::endl;
+    std::cout << "  Num Input Layers: " << (nn->input).size() << std::endl;
+    std::cout << "  Num Weights: " << (nn->weights).size() << std::endl;
 
     nn->reset();
 
     std::cout << "Making the Code Generator object" << std::endl;
-	CodeGenerator* code_gen = new CodeGenerator(nn, "../templates/esp32", "../example_out");
-	std::cout << "Generating Layer and Weight stuff" << std::endl;
-	code_gen->generate();
-	std::cout << "Dumping the code" << std::endl;
-	code_gen->dump();
+    CodeGenerator* code_gen = new CodeGenerator(nn, "../templates/esp32", "../example_out");
+    std::cout << "Generating Layer and Weight stuff" << std::endl;
+    code_gen->generate();
+    std::cout << "Dumping the code" << std::endl;
+    code_gen->dump();
 
-  delete nn;
-
+    delete nn;
+    return 0;
 }
