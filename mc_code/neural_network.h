1b84a847751949b1efbe829d13c41c087f24e869
sarahaguasvivas <saag5228@colorado.edu>
2019-09-08 08:48:11 -0600

    creating new templates

--- mc_code/neural_network.h
+++ mc_code/neural_network.h
@@ -6,36 +6,28 @@ volatile int num_layers= 0;
 struct Conv1D {
     int kernel_size;
     int filters;
-    int output_shape;
-    float **h;
-    int input_sh1;
-    int input_sh2;
+    int strides;
+    int weight_shape[3];
+    int output_shape[2];
+    int input_shape[2];
 };
 
 struct MaxPooling1D {
     int pool_size;
-    int input_shape;
-    int output_shape;
+    int input_shape[2];
+    int output_shape[2];
     int strides;
-    float **h;
-    int input_sh1;
-    int input_sh2;
 };
 
 struct Dense {
-    int input_size;
-    int output_size;
+    int input_shape[2];
+    int output_shape[2];
     char activation;
-    float *h;
 };
 
 struct Flatten2D1D {
-    int in_shape_0;
-    int in_shape_1;
-    float *h;
-    int output_size;
-    int input_sh1;
-    int input_sh2;
+    int input_shape[2];
+    int output_shape;
 };
 
 
