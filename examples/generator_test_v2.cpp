825e1e20e9a37f45f7572fa2df97c229e84a4adf
sarahaguasvivas <saag5228@colorado.edu>
2019-09-12 12:11:50 -0600

    fixed shape readers

--- examples/generator_test_v2.cpp
+++ examples/generator_test_v2.cpp
@@ -89,6 +89,7 @@ NeuralNetwork* makeNet()
   dense_layer->b = w4;
   dense_layer->input_shape.push_back(1);
   dense_layer->input_shape.push_back(2);
+  dense_layer->activation="linear";
   dense_layer->units = 5;
 
   input_layer->layer_type = "input";
