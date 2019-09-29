c68a9555a7f7374ab029bf67bdc27867e507f21c
sarahaguasvivas <saag5228@colorado.edu>
2019-09-29 17:55:23 -0600

    finished templates

---
+++ example_out/include/layers/dense.h
@@ -0,0 +1,30 @@
+
+/*************
+* dense.h
+*
+* Dense Layer Header
+* Hardware Plateform: ESP-32
+*
+* This file defines the datastructure needed to create a Dense (Fully Connected) layer.
+*/
+
+#ifndef __DENSE_H__
+#define __DENSE_H__
+
+
+struct Dense {
+	// Weights and biases defining the layer
+	const float* weights;			// Pointer to first element of weights array (should be 'const float' in weight.best.hdf5)
+	const float* biases;				// Pointer to constant bias
+
+    char activation;  // in our case this delimiter is a char that indicates which activation function to call
+
+    int weight_shape[2];
+
+	// Shape of the input and output
+	int input_shape[1];
+	int output_shape[1];
+};
+
+#endif
+
---
+++ example_out/include/layers/maxpool2d.h
@@ -0,0 +1,27 @@
+
+/*************
+* maxpool1d.h
+*
+* MaxPool1D Layer Header
+* Hardware Plateform: ESP-32
+*
+* This file defines the datastructure needed to create a 1D MaxPooling layer.
+*/
+
+#ifndef __MAXPOOL1D_H__
+#define __MAXPOOL1D_H__
+
+struct MaxPooling2D {
+	// Kernel size
+	int pool_size[2];	// How many entries to consider
+	int strides[2];			// How many to skip	
+
+	// Shape of the input and output
+	int input_shape[2];		// (INPUT_SIZE x NUM_INPUT_CHANNELS)
+	int output_shape[2];	// (OUTPUT_SIZE x NUM_OUTPUT_CHANNELS)
+};
+struct MaxPooling2D buildMaxPooling2D(int, int , int, int , int, int, int);
+
+float * fwdMaxPooling2D(struct MaxPooling2D , float *);
+#endif
+
