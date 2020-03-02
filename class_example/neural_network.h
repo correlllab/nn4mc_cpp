7dd9f99602ffa37ea889201021253a36c1a11fa5
sarahaguasvivas <saag5228@colorado.edu>
2020-03-02 09:22:22 -0700

    removing unnecessary output files

---
+++ class_example/neural_network.h
@@ -0,0 +1,32 @@
+
+/********************
+
+    NEURAL_NETWORK_H
+
+Code Generated using nn4mc
+
+*/
+
+#ifndef __NEURAL_NETWORK_H__
+#define __NEURAL_NETWORK_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#include "neural_network_params.h"
+#include "dense.h"
+#include "dense.h"
+#include "dense.h"
+
+
+struct Dense dense_39;
+struct Dense dense_40;
+struct Dense dense_41;
+
+
+
+
+#endif
+
