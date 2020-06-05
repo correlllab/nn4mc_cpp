ea6187a4e4216d007ac4e08ddfd15f6e289b6447
sarahaguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2020-06-05 06:34:35 -0600

    setup scripts

--- scripts/setup_mac_osx.sh
+++ scripts/setup_mac_osx.sh
@@ -7,15 +7,20 @@ if ! -d "/usr/local/include"
     then
         echo "Creating /usr/local/include" && sudo mkdir /usr/local/include && sudo chown -R `whoami`:admin /usr/local/include
 fi
+
 brew install cmake
 brew install hdf5
 brew link hdf5
 brew install boost
+
 echo 'export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:"/usr/local/Cellar/hdf5/1.12.0/include"' >> ~./bashrc
 echo 'export LD_LIBRARY_PATH=$LIBRARY_PATH:"/usr/local/Cellar/hdf5/1.12.0/lib"' >> ~/.bashrc
 source ~/.bashrc
 brew tap nlohmann/json
 brew install nlohmann_json
+cd ../lib
+git clone https://github.com/nlohmann/json nlohmann_json
+cd ..
 
 
 
