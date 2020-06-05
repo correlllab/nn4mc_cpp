aa3142117fc06a7f2edda3ec548538664cf81070
sarahaguasvivas <sarahaguasvivas@Sarahs-MacBook-Pro.local>
2020-06-05 07:39:45 -0600

    setup scripts

--- scripts/setup_linux_mint.sh
+++ scripts/setup_linux_mint.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 echo "Installing dependencies for nn4mc"
-echo 'export NN4MC_HOME="$PWD"' >> ~/.bashrc
-#sudo ln -s libhdf5_serial.so.8.0.2 libhdf5.so
-#sudo ln -s libhdf5_serial_hl.so.8.0.2 libhdf5_hl.so
 sudo apt-get install libhdf5-dev
+conda install -c anaconda hdf5
 sudo apt-get install libboost-all-dev
-cd $NN4MC_HOME/lib
+cd ../lib
 git clone https://github.com/nlohmann/json nlohmann_json
-cd $NN4MC_HOME
+cd ..
 
--- scripts/setup_mac_osx.sh
+++ scripts/setup_mac_osx.sh
@@ -3,11 +3,10 @@ echo "Installing dependencies for NN4MC"
 # Quick check that we actually have brew, if not, lets install it
 command -v brew >/dev/null 2>&1 || echo >&2 "Homebrew is missing, you can install it by running \n/usr/bin/ruby -e \$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || exit 1
 
-if ! -d "/usr/local/include"
-    then
-        echo "Creating /usr/local/include" && sudo mkdir /usr/local/include && sudo chown -R `whoami`:admin /usr/local/include
-fi
-
+#if ! -d "/usr/local/include"
+#    then
+#        echo "Creating /usr/local/include" && sudo mkdir /usr/local/include && sudo chown -R `whoami`:admin /usr/local/include
+#fi
 brew install cmake
 brew install hdf5
 brew link hdf5
