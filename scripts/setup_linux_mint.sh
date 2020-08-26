63c1f0384563243056f691fec831bd82c57b92fc
sarahaguasvivas <saag5228@colorado.edu>
2020-08-26 11:53:12 -0600

    fixed CMakeLists issue

--- scripts/setup_linux_mint.sh
+++ scripts/setup_linux_mint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "Installing dependencies for nn4mc"
+sudo apt-get update
 sudo apt-get install libhdf5-dev
-conda install -c anaconda hdf5
 sudo apt-get install libboost-all-dev
 cd ../lib
 git clone https://github.com/nlohmann/json nlohmann_json
 cd ..
+sudo apt-get install build-essential
+
 
