36cacd21a3547f90ebe79d96b2ecd2ad96bfdc79
sarahaguasvivas <saag5228@colorado.edu>
2020-08-31 08:48:48 -0600

    downgraded required version of cmake

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -6,14 +6,14 @@ sudo apt-get install libboost-all-dev
 cd lib
 git clone https://github.com/nlohmann/json nlohmann_json
 cd ..
+sudo apt install cmake
 sudo apt-get install build-essential
 sudo apt-get install manpages-dev
 sudo apt-get instalal hdf5-tools
-sudo ldconfig
 mkdir build && cd build
 cmake ..
 make
- 
+
 
 
 
