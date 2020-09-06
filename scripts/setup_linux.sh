b6ce7d4ef3b198ada6a5dba3ef701e809db330a3
Sarah Aguasvivas <31457827+sarahaguasvivas@users.noreply.github.com>
2020-09-06 06:40:24 -0600

    Update setup_linux.sh

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 echo "Installing dependencies for nn4mc"
-sudo apt-get update
-sudo apt-get install libhdf5-serial-deva ccache libopenmpi-dev
-sudo apt-get install libboost-all-dev
+sudo apt-get update # updating sudo apt-get 
+sudo apt-get install libhdf5-serial-dev # installing hdf5
+sudo apt-get install libboost-all-dev # installing boost library
 cd lib
-git clone https://github.com/nlohmann/json nlohmann_json
+git clone https://github.com/nlohmann/json nlohmann_json # cloning nlohmann json wrapper
 cd ..
-sudo apt install cmake
-sudo apt-get install build-essential
-sudo apt-get install manpages-dev
-sudo apt-get instalal hdf5-tools
+sudo apt install cmake # just in case they don't have it; won't install if they have it
+sudo apt-get install build-essential  
+#sudo apt-get install manpages-dev
 mkdir build && cd build
 cmake ..
 make
